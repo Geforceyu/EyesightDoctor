37d38ab76b7ce86a20947571b26e3289d7e3844b
Chonghua Yu <>
2018-09-25 17:14:46 +0800

    proj init

---
+++ EyesightDoctor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EyesightDoctor
+//
+//  Created by Chonghua Yu on 2018/9/25.
+//  Copyright © 2018年 Chonghua Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
